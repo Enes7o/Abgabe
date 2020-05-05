ba04d6bc2b8904ee00882aa2d0f844acf0c43af2
Enes7o <64870364+Enes7o@users.noreply.github.com>
2020-05-05 23:46:49 +0200

    Add files via upload

---
+++ untitled2/src/test/java/JunitTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JunitTest {
+    @Test
+ public void fill() {
+     int value1 = 10;
+     int value2 = 20;
+     String value3 = "+";
+     System.out.println(value1);
+     System.out.println(value2);
+     System.out.println(value3);
+ }
+}
---
+++ untitled2/src/test/java/mainfunctionsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class mainfunctionsTest {
+
+    @Test
+    public void fill() {
+        int value1 = 10;
+        int value2 = 20;
+        String value3 = "+";
+        System.out.println(value1);
+        System.out.println(value2);
+        System.out.println(value3);
+}}
